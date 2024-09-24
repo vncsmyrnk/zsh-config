85f70cc6eb7716b0c9d2d58f4cb3806d92d21ad7
Vinicius Mayrink <vmayrink12@gmail.com>
2024-09-24 19:19:46 -0300

    fix: making name and email be set as literal values

--- home/utils/git-basic-config.zsh
+++ home/utils/git-basic-config.zsh
@@ -11,7 +11,7 @@ if [ -z "$name" ] || [ -z "$email" ]; then
   exit 1
 fi
 
-git config --global user.name $name
-git config --global user.email $email
+git config --global user.name "$name"
+git config --global user.email "$email"
 git config --global --add safe.directory '*' # Fixing the dubious ownership issue
 git config --global pull.ff true # Fixing merge on pull requests
