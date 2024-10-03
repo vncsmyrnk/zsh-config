695212144c4f16d367ce4e519f50b0bfe19da34a
Vinicius Mayrink <vmayrink12@gmail.com>
2024-10-03 17:34:32 -0300

    feat: grep kill script

---
+++ home/utils/grep-kill.zsh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+# Kills a process by name
+# Similar to pkill but with grep
+# filtering
+
+filter=$1
+
+if [ -z "$filter" ]; then
+  echo "Usage: grep-kill <filter>"
+  exit 1
+fi
+
+ps aux | grep -e $filter | awk '{ print $2 }' | xargs kill -9
