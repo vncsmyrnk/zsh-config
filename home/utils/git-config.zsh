763955f52923bf0a4ba22fae54618b92276038a1
Vinicius Mayrink <vmayrink12@gmail.com>
2025-01-13 21:29:37 -0300

    fix: renames git-basic-config and regenerates ssh key

--- home/utils/git-config.zsh
+++ home/utils/git-config.zsh
@@ -7,11 +7,14 @@ name=$1
 email=$2
 
 if [ -z "$name" ] || [ -z "$email" ]; then
-  echo "Usage: git-basic-setup.sh <name> <email>"
+  echo "Usage: git-config <name> <email>"
   exit 1
 fi
 
 git config --global user.name "$name"
 git config --global user.email "$email"
 git config --global --add safe.directory '*' # Fixing the dubious ownership issue
-git config --global pull.ff true # Fixing merge on pull requests
+git config --global pull.ff true             # Fixing merge on pull requests
+
+ssh-keygen -t ed25519 -C "$email"
+xclip -selection clipboard <~/.ssh/id_ed25519.pub
